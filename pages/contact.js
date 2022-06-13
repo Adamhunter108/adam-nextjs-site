3d3d0adf307a69e4709f20e539dc39d1f5942a9f
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2022-06-13 15:15:40 -0400

    Replaced labels for placeholders on contact form

--- pages/contact.js
+++ pages/contact.js
@@ -166,11 +166,11 @@ export default function Contact() {
                                     />
                                 </div>
 
-                                <label
+                                {/* <label
                                     htmlFor="fullname" 
                                     className="text-black font-light mt-8">
                                         Full name<span className="text-red-500">*</span>
-                                </label>
+                                </label> */}
                                 <input 
                                     type="text" 
                                     value={fullname}
@@ -178,69 +178,73 @@ export default function Contact() {
                                         setFullname(e.target.value)
                                     }}
                                     name="fullname"
-                                    className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-indigo-500 font-light text-gray-800"
+                                    placeholder="Your name"
+                                    className="bg-transparent border-b mt-4 py-3 pl-4 focus:outline-none focus:rounded-md focus:ring-2 ring-indigo-500 font-light text-gray-800"
                                 />
                                 {errors?.fullname && (
-                                    <p className="text-red-500">Name cannot be empty.</p>
+                                    <p className="text-red-500">I need something to call you</p>
                                 )}
 
-                                <label
+                                {/* <label
                                     htmlFor="email"
                                     className="text-black font-light mt-4">
                                         E-mail<span className="text-red-500">*</span>
-                                </label>
+                                </label> */}
                                 <input
                                     type="email"
                                     name="email"
                                     value={email}
+                                    placeholder="Your email"
                                     onChange={(e) => {
                                         setEmail(e.target.value)
                                     }} 
-                                    className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-indigo-500 font-light text-gray-800"
+                                    className="bg-transparent border-b mt-4 py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-2 ring-indigo-500 font-light text-gray-800"
                                 />
                                 {errors?.email && (
-                                    <p className="text-red-500">Email cannot be empty.</p>
+                                    <p className="text-red-500">I need somewhere to hit you back</p>
                                 )}
 
-                                <label 
+                                {/* <label 
                                     htmlFor="subject" 
                                     className="text-black font-light mt-4">
                                         Subject<span className="text-red-500">*</span>
-                                </label>
+                                </label> */}
                                 <input 
                                     type="text"
                                     name="subject"
                                     value={subject}
+                                    placeholder="Subject"
                                     onChange={(e) => {
                                         setSubject(e.target.value)
                                     }} 
-                                    className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-indigo-500 font-light text-gray-800"
+                                    className="bg-transparent border-b mt-4 py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-2 ring-indigo-500 font-light text-gray-800"
                                 />
                                 {errors?.subject && (
-                                    <p className="text-red-500">Subject cannot be empty.</p>
+                                    <p className="text-red-500">TLDR it for me</p>
                                 )}
 
-                                <label 
+                                {/* <label 
                                     htmlFor="message" 
                                     className="text-black font-light mt-4">
                                         Message<span className="text-red-500">*</span>
-                                </label>
+                                </label> */}
                                 <textarea 
                                     name="message"
                                     value={message}
+                                    placeholder="Message"
                                     onChange={(e) => {
                                         setMessage(e.target.value)
                                     }} 
-                                    className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-indigo-500 font-light text-gray-800">
+                                    className="bg-transparent border-b mt-4 py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-2 ring-indigo-500 font-light text-gray-800">
                                 </textarea>
                                 {errors?.message && (
-                                    <p className="text-red-500">Message cannot be empty.</p>
+                                    <p className="text-red-500">What do you want to say?</p>
                                 )}
 
                                 <div className="flex flex-row items-center justify-center">
                                     <button
                                         type="submit" 
-                                        className="px-10 mt-8 py-2 bg-gradient-to-b from-indigo-800 via-indigo-600 to-indigo-400 text-gray-50 hover:bg-gradient-to-t from-indigo-800 via-indigo-600 to-indigo-400 hover:text-black font-light rounded-md text-lg flex flex-row items-center"
+                                        className="px-10 mt-8 py-2 bg-gradient-to-b from-indigo-800 via-indigo-600 to-indigo-400 text-gray-50 hover:bg-gradient-to-t from-indigo-800 via-indigo-600 to-indigo-400 hover:text-gray-400 font-light rounded-md text-lg flex flex-row items-center"
                                         >
                                         {buttonText}
                                         <svg width="24" height="24" className="text-white ml-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M511.6 36.86l-64 415.1c-1.5 9.734-7.375 18.22-15.97 23.05c-4.844 2.719-10.27 4.097-15.68 4.097c-4.188 0-8.319-.8154-12.29-2.472l-122.6-51.1l-50.86 76.29C226.3 508.5 219.8 512 212.8 512C201.3 512 192 502.7 192 491.2v-96.18c0-7.115 2.372-14.03 6.742-19.64L416 96l-293.7 264.3L19.69 317.5C8.438 312.8 .8125 302.2 .0625 289.1s5.469-23.72 16.06-29.77l448-255.1c10.69-6.109 23.88-5.547 34 1.406S513.5 24.72 511.6 36.86z"/></svg>
